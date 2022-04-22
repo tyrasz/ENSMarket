7a073e8a7a1383fcb532b26f88d9d40c9235fc39
Zhikai <wzhikai@gmail.com>
2022-04-23 01:33:47 +0200

    refactored test

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,22 +2,17 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 beforeEach(async() => {
+  const ENSMarket = await ethers.getContractFactory("ENSMarket");
+  ensMarket = await ENSMarket.deploy();
 
+  const MockENS = await ethers.getContractFactory("MockENS");
+  const mockENS = await MockENS.deploy();
+  await mockENS.deployed();
 });
 
 describe("ENSMarket", function () {
   it("Should deploy a ENSMarket contract", async function () {
-    const ENSMarket = await ethers.getContractFactory("ENSMarket");
-    const ensMarket = await ENSMarket.deploy();
-    await ensMarket.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const contractAddress = ensMarket.address;
+    console.log(contractAddress);
   });
 });
