cf70f361369feed5c3af8040e3830d23905d8237
Zhikai <wzhikai@gmail.com>
2022-04-23 01:21:32 +0200

    added the ENSMarket, MockENS contracts

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ENSMarket = await hre.ethers.getContractFactory("ENSMarket");
+  const ensMarket = await ENSMarket.deploy();
 
-  await greeter.deployed();
+  await ensMarket.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("ENSMarket deployed to:", ensMarket.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
