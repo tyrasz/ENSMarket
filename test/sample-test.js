58493cf90d4856ac5a3280647495f9a4026a8bd8
Kai Morciniec <kaimorciniec@gmail.com>
2022-04-23 15:16:32 +0100

    add rinkeby network

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,27 +2,20 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 
-beforeEach(async() => {
-  const ENSMarket = await ethers.getContractFactory("ENSMarket");
-  ensMarket = await ENSMarket.deploy();
-
-  //deploy ENS on local
-  const ENSRegistry = await ethers.getContractFactory("LOCALENSRegistry")
-  ensSetup = await ENSRegistry.deploy();
-  // const ENSProvider = await provider.setupENS();
-  // ensRegistry = await ENSRegistry.deploy()
-
-  
-});
 
 describe("ENSMarket", function () {
-  it("Should deploy a ENSMarket contract", async function () {
-    const contractAddress = ensMarket.address;
-    console.log(contractAddress);
-  });
 
-  it("Should deploy a ENSRegistry contract", async function () {
-    const contractAddress2 = ensSetup.address;
-    console.log(contractAddress2);
-  });
+    beforeEach(async() => {
+        const ENSMarket = await ethers.getContractFactory("ENSMarket");
+        ensMarket = await ENSMarket.deploy();
+        await ensMarket.deployed();
+        [lister] = await ethers.getSigners();
+    });
+
+    it("Should deploy a ENSMarket contract", async function () {
+        const contractAddress = ensMarket.address;
+        console.log(contractAddress);
+    });
+
 });
+
