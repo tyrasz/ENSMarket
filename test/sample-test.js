5866ef36da451614212dff86ebbe36dca71096b3
Zhikai <wzhikai@gmail.com>
2022-04-23 13:10:22 +0200

    added the registry contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,28 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
 beforeEach(async() => {
   const ENSMarket = await ethers.getContractFactory("ENSMarket");
   ensMarket = await ENSMarket.deploy();
 
-  const MockENS = await ethers.getContractFactory("MockENS");
-  const mockENS = await MockENS.deploy();
-  await mockENS.deployed();
+  //deploy ENS on local
+  const ENSRegistry = await ethers.getContractFactory("LOCALENSRegistry")
+  ensSetup = await ENSRegistry.deploy();
+  // const ENSProvider = await provider.setupENS();
+  // ensRegistry = await ENSRegistry.deploy()
+
+  
 });
 
 describe("ENSMarket", function () {
   it("Should deploy a ENSMarket contract", async function () {
     const contractAddress = ensMarket.address;
     console.log(contractAddress);
+  });
+
+  it("Should deploy a ENSRegistry contract", async function () {
+    const contractAddress2 = ensSetup.address;
+    console.log(contractAddress2);
   });
 });
